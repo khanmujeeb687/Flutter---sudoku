b2866bd0d369b17cf86f190fa3e14c961fcbc2e9
mujeeb <khanmujeen687@gmail.com>
2021-04-02 09:14:07 +0530

    first version released

--- android/app/src/main/kotlin/com/example/sudokutable/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sudokutable
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/mintech/sudokutable/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mintech.sudokutable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
